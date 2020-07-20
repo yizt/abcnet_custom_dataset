a8b16f5801fb169e8b82c33932b502066bfd2aff
yizt <csuyzt@163.com>
2020-07-20 11:30:14 +0800

    生成coco格式的json文件

--- trans_ic15.py
+++ trans_ic15.py
@@ -7,8 +7,10 @@
 """
 import codecs
 import glob
+import json
 import os
 
+import cv2
 import numpy as np
 from scipy import interpolate
 
@@ -91,6 +93,144 @@ def save_to_abc(txt_path, image_annotation):
             w.write('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}||||{}\n'.format(*points, text))
 
 
+def ic15_to_abc(annotation_dir, image_dir, abc_gt_dir):
+    """
+    icdar15数据转为abcnet标注格式(带bezier控制点)
+    :param annotation_dir:
+    :param image_dir:
+    :param abc_gt_dir:
+    :return:
+    """
+    for ann_name in os.listdir(annotation_dir):
+        ann_path = os.path.join(annotation_dir, ann_name)
+        ann_info = load_ic15(ann_path, image_dir)
+
+        dst_gt_path = os.path.join(abc_gt_dir,
+                                   '{}.txt'.format(os.path.splitext(ann_info['file_name'])[0]))
+        save_to_abc(dst_gt_path, ann_info)
+
+
+def gen_abc_json(abc_gt_dir, abc_json_path, image_dir):
+    """
+    根据abcnet的gt标注生成coco格式的json标注
+    :param abc_gt_dir:
+    :param abc_json_path:
+    :param image_dir:
+    :return:
+    """
+    # Desktop Latin_embed.
+    cV2 = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4',
+           '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
+           '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+           'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
+
+    dataset = {
+        'licenses': [],
+        'info': {},
+        'categories': [],
+        'images': [],
+        'annotations': []
+    }
+    with open('./classes.txt') as f:
+        classes = f.read().strip().split()
+    for i, cls in enumerate(classes, 1):
+        dataset['categories'].append({
+            'id': i,
+            'name': cls,
+            'supercategory': 'beverage',
+            'keypoints': ['mean',
+                          'xmin',
+                          'x2',
+                          'x3',
+                          'xmax',
+                          'ymin',
+                          'y2',
+                          'y3',
+                          'ymax',
+                          'cross']  # only for BDN
+        })
+
+    def get_category_id(cls):
+        for category in dataset['categories']:
+            if category['name'] == cls:
+                return category['id']
+
+    # 遍历abcnet txt 标注
+    indexes = sorted([f.split('.')[0]
+                      for f in os.listdir(abc_gt_dir)])
+
+    j = 1
+    for index in indexes:
+        # if int(index) >3: continue
+        print('Processing: ' + index)
+        im = cv2.imread(os.path.join(image_dir) + index + '.jpg')
+        height, width, _ = im.shape
+        dataset['images'].append({
+            'coco_url': '',
+            'date_captured': '',
+            'file_name': index + '.jpg',
+            'flickr_url': '',
+            'id': int(index),
+            'license': 0,
+            'width': width,
+            'height': height
+        })
+        anno_file = os.path.join(abc_gt_dir) + index + '.txt'
+
+        with open(anno_file) as f:
+            lines = [line for line in f.readlines() if line.strip()]
+            for i, line in enumerate(lines):
+                pttt = line.strip().split('||||')
+                parts = pttt[0].split(',')
+                ct = pttt[-1].strip()
+
+                cls = 'text'
+                segs = [float(kkpart) for kkpart in parts[:16]]
+
+                xt = [segs[ikpart] for ikpart in range(0, len(segs), 2)]
+                yt = [segs[ikpart] for ikpart in range(1, len(segs), 2)]
+                xmin = min([xt[0], xt[3], xt[4], xt[7]])
+                ymin = min([yt[0], yt[3], yt[4], yt[7]])
+                xmax = max([xt[0], xt[3], xt[4], xt[7]])
+                ymax = max([yt[0], yt[3], yt[4], yt[7]])
+                width = max(0, xmax - xmin + 1)
+                height = max(0, ymax - ymin + 1)
+                if width == 0 or height == 0:
+                    continue
+
+                max_len = 100
+                recs = [len(cV2) + 1 for ir in range(max_len)]
+
+                ct = str(ct)
+                print('rec', ct)
+
+                for ix, ict in enumerate(ct):
+                    if ix >= max_len: continue
+                    if ict in cV2:
+                        recs[ix] = cV2.index(ict)
+                    else:
+                        recs[ix] = len(cV2)
+
+                dataset['annotations'].append({
+                    'area': width * height,
+                    'bbox': [xmin, ymin, width, height],
+                    'category_id': get_category_id(cls),
+                    'id': j,
+                    'image_id': int(index),
+                    'iscrowd': 0,
+                    'bezier_pts': segs,
+                    'rec': recs
+                })
+                j += 1
+    # 写入json文件
+    folder = os.path.dirname(abc_json_path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    with open(abc_json_path, 'w') as f:
+        json.dump(dataset, f)
+
+
 def test_load_ic15():
     anno_path = '/Users/yizuotian/dataset/IC15/ch4_training_localization_transcription_gt/gt_img_1.txt'
     img_dir = '/Users/yizuotian/dataset/IC15/ch4_training_images'
@@ -99,5 +239,17 @@ def test_load_ic15():
     save_to_abc(txt_path, load_ic15(anno_path, img_dir))
 
 
+def test_ic15_to_abc():
+    ann_dir = '/Users/yizuotian/dataset/IC15/ch4_training_localization_transcription_gt'
+    img_dir = '/Users/yizuotian/dataset/IC15/ch4_training_images'
+    txt_dir = '/Users/yizuotian/dataset/IC15/abcnet_gt_train'
+    json_path = '/Users/yizuotian/dataset/IC15/annotations/train.json'
+    os.makedirs(txt_dir, exist_ok=True)
+    # ic15_to_abc(ann_dir, img_dir, txt_dir)
+
+    gen_abc_json(txt_dir, json_path, img_dir)
+
+
 if __name__ == '__main__':
-    test_load_ic15()
+    # test_load_ic15()
+    test_ic15_to_abc()
